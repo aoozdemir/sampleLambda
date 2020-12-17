5282071c24f0329c91c56c156647df116e32c691
Oguzhan Ozdemir <aoozdemir@live.com>
2020-12-17 18:15:10 +0300

    stash

--- api/s3Upload.js
+++ api/s3Upload.js
@@ -5,41 +5,79 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const s3Bucket = process.env['S3_BUCKET'];
 
-const thundra = require("@thundra/core")();
+const thundra = require("@thundra/core");
+// const thundra = require('../thundra-lambda-agent-nodejs/dist/thundra');
 
-module.exports.push = thundra((event, _, callback) => {
-  const object = {
-    MessageId: event.Records[0].messageId,
-    Attributes: event.Records[0].attributes,
-    Body: JSON.parse(event.Records[0].body),
-  };
+// const TimeAwareSampler = thundra.samplers.TimeAwareSampler;
 
-  const buffer = Buffer.from(JSON.stringify(object));
+// const config = {
+//   invocationConfig: {
+//     sampler: new TimeAwareSampler(300000) // Sample metrics every 300s
+//   },
+// };
+const config = {};
 
-  const params = {
-    Bucket: s3Bucket,
-    Key: `${event.Records[0].messageId}.json`,
-    Body: buffer,
-    ContentEncoding: 'base64',
-    ContentType: 'application/json',
-    ACL: 'public-read',
-  };
+module.exports.push = thundra(config)((event, _, callback) => {
+  try {
+    const object = {
+      MessageId: event.Records[0].messageId,
+      Attributes: event.Records[0].attributes,
+      Body: JSON.parse(event.Records[0].body),
+    };
 
-  s3.putObject(params, function (err, _) {
-    if (err) {
-      console.log(err, err.stack);
-      callback(new Error('Couldn\'t send the document to S3.'));
-      return;
-    } else {
-      console.log('Successfully sent the document to S3.');
-
+    if (object.Body.raiseError) {
       callback(null, {
-        statusCode: 200,
+        statusCode: 404,
         body: JSON.stringify({
-          message: 'Successfully sent the document to S3.'
+          message: 'NOT_FOUND'
+        })
+      });
+      return;
+    } else if (!object.Body.timeOut) {
+      const buffer = Buffer.from(JSON.stringify(object));
+
+      const params = {
+        Bucket: s3Bucket,
+        Key: `${event.Records[0].messageId}.json`,
+        Body: buffer,
+        ContentEncoding: 'base64',
+        ContentType: 'application/json',
+        ACL: 'public-read',
+      };
+
+      s3.putObject(params, function (err, _) {
+        if (err) {
+          console.log(err, err.stack);
+          callback(new Error('Couldn\'t send the document to S3.'));
+          return;
+        } else {
+          console.log('Successfully sent the document to S3.');
+
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+              message: 'Successfully sent the document to S3.'
+            })
+          });
+          return;
+        }
+      });
+    } else {
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'TIMED_OUT'
         })
       });
       return;
     }
-  });
+  } catch (e) {
+    callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'BAD_REQUEST'
+      })
+    });
+    return;
+  }
 });
--- api/sqsWrite.js
+++ api/sqsWrite.js
@@ -5,30 +5,35 @@ const AWS = require('aws-sdk');
 const sqsQueue = new AWS.SQS();
 const sqsUrl = process.env['SQS_URL'];
 
-const thundra = require("@thundra/core")();
+const thundra = require("@thundra/core");
+// const thundra = require('../thundra-lambda-agent-nodejs/dist/thundra');
+// const TimeAwareSampler = thundra.samplers.TimeAwareSampler;
+const config = {};
 
-module.exports.push = thundra((event, _, callback) => {
-  const params = {
-    MessageBody: event.body,
-    QueueUrl: sqsUrl,
-  };
-  console.log(params);
+module.exports.push = thundra(config)(
+  (event, _, callback) => {
+    const params = {
+      MessageBody: event.body,
+      QueueUrl: sqsUrl,
+    };
+    console.log(params);
 
-  sqsQueue.sendMessage(params, (err, _) => {
-    if (err) {
-      console.error(err);
-      callback(new Error('Couldn\'t send the message to SQS.'));
-      return;
-    } else {
-      console.log('Successfully sent the message to SQS.');
+    sqsQueue.sendMessage(params, (err, _) => {
+      if (err) {
+        console.error(err);
+        callback(new Error('Couldn\'t send the message to SQS.'));
+        return;
+      } else {
+        console.log('Successfully sent the message to SQS.');
 
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'Successfully sent the message to SQS.'
-        })
-      });
-      return;
-    }
-  });
-});
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: 'Successfully sent the message to SQS.'
+          })
+        });
+        return;
+      }
+    });
+  }
+);
